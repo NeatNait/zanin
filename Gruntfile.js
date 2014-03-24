4c9c6f483b0d5e487ef303062d3c9ad22f70b599
Alvaro Pinot <alvaro@neatnait.com>
2014-03-24 13:38:29 +0100

    fixed background image again

--- Gruntfile.js
+++ Gruntfile.js
@@ -186,7 +186,8 @@ module.exports = function (grunt) {
           src: [
             '<%= yeoman.dist %>/scripts/{,*/}*.js',
             '<%= yeoman.dist %>/styles/{,*/}*.css',
-            '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+            //TODO reanable for images
+            //'<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
             '<%= yeoman.dist %>/styles/fonts/*'
           ]
         }
