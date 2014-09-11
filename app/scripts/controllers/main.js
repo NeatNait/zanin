8a6d787e73cb9013ee7bf2ecd4046f363e24df89
triskbit <carlos@neatnait.com>
2014-09-11 21:47:27 +0200

    added more achievements objects

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -84,6 +84,46 @@ angular.module('zaninApp')
 					{name:'combo100', desc:'100 combo'},
 					{name:'combo250', desc:'250 combo'},
 					{name:'combo500', desc:'500 combo'},
+					{name:'red25', desc:'25 red'},
+					{name:'red50', desc:'50 red'},
+					{name:'red100', desc:'100 red'},
+					{name:'red250', desc:'250 red'},
+					{name:'red500', desc:'500 red'},
+					{name:'blue25', desc:'25 blue'},
+					{name:'blue50', desc:'50 blue'},
+					{name:'blue100', desc:'100 blue'},
+					{name:'blue250', desc:'250 blue'},
+					{name:'blue500', desc:'500 blue'},
+					{name:'yellow25', desc:'25 yellow'},
+					{name:'yellow50', desc:'50 yellow'},
+					{name:'yellow100', desc:'100 yellow'},
+					{name:'yellow250', desc:'250 yellow'},
+					{name:'yellow500', desc:'500 yellow'},
+					{name:'green25', desc:'25 green'},
+					{name:'green50', desc:'50 green'},
+					{name:'green100', desc:'100 green'},
+					{name:'green250', desc:'250 green'},
+					{name:'green500', desc:'500 green'},
+					{name:'tap25', desc:'25 tap'},
+					{name:'tap50', desc:'50 tap'},
+					{name:'tap100', desc:'100 tap'},
+					{name:'tap250', desc:'250 tap'},
+					{name:'tap500', desc:'500 tap'},
+					{name:'doubleTap25', desc:'25 doubleTap'},
+					{name:'doubleTap50', desc:'50 doubleTap'},
+					{name:'doubleTap100', desc:'100 doubleTap'},
+					{name:'doubleTap250', desc:'250 doubleTap'},
+					{name:'doubleTap500', desc:'500 doubleTap'},
+					{name:'swipeRight25', desc:'25 swipeRight'},
+					{name:'swipeRight50', desc:'50 swipeRight'},
+					{name:'swipeRight100', desc:'100 swipeRight'},
+					{name:'swipeRight250', desc:'250 swipeRight'},
+					{name:'swipeRight500', desc:'500 swipeRight'},
+					{name:'swipeLeft25', desc:'25 swipeLeft'},
+					{name:'swipeLeft50', desc:'50 swipeLeft'},
+					{name:'swipeLeft100', desc:'100 swipeLeft'},
+					{name:'swipeLeft250', desc:'250 swipeLeft'},
+					{name:'swipeLeft500', desc:'500 swipeLeft'},
 					{name:'tps4', desc:'4 tps'},
 					{name:'tps5', desc:'5 tps'},
 					{name:'tps6', desc:'6 tps'},
@@ -734,19 +774,19 @@ angular.module('zaninApp')
 			}
 
 			//Taps per second
-			if($scope.game.tapsPerSecond >= 4 && $scope.game.ponits >= 1000 && checkPlayerAchievement('tps4',achievements)){
+			if($scope.game.tapsPerSecond >= 4 && $scope.game.points >= 1000 && checkPlayerAchievement('tps4',achievements)){
 				$scope.game.metAchievements.push('tps4');
 				achievements.push('tps4');
-			} else if($scope.game.tapsPerSecond >= 5 && $scope.game.ponits >= 2000 && checkPlayerAchievement('tps5',achievements)){
+			} else if($scope.game.tapsPerSecond >= 5 && $scope.game.points >= 2000 && checkPlayerAchievement('tps5',achievements)){
 				$scope.game.metAchievements.push('tps5');
 				achievements.push('tps5');
-			} else if($scope.game.tapsPerSecond >= 6 && $scope.game.ponits >= 3000 && checkPlayerAchievement('tps6',achievements)){
+			} else if($scope.game.tapsPerSecond >= 6 && $scope.game.points >= 3000 && checkPlayerAchievement('tps6',achievements)){
 				$scope.game.metAchievements.push('tps6');
 				achievements.push('tps6');
-			}else if($scope.game.tapsPerSecond >= 8 && $scope.game.ponits >= 6000 && checkPlayerAchievement('tps8',achievements)){
+			}else if($scope.game.tapsPerSecond >= 8 && $scope.game.points >= 6000 && checkPlayerAchievement('tps8',achievements)){
 				$scope.game.metAchievements.push('tps8');
 				achievements.push('tps8');
-			}else if($scope.game.tapsPerSecond >= 10 && $scope.game.ponits >= 7000 && checkPlayerAchievement('tps10',achievements)){
+			}else if($scope.game.tapsPerSecond >= 10 && $scope.game.points >= 7000 && checkPlayerAchievement('tps10',achievements)){
 				$scope.game.metAchievements.push('tps10');
 				achievements.push('tps10');
 			}
